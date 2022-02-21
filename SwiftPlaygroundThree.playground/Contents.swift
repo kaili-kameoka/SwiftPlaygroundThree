a73e556ca87c1f093bd47def1c3819b0eb667f07
Kaili Kameoka <kaili.kameoka@gmail.com>
2022-02-21 13:02:58 -1000

    Completed parts 1 - 7

---
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var fruitNames: [String];
+fruitNames = ["Kiwi", "Apples", "Watermelon"];
+
+print(fruitNames)
+
+print("I like to eat \(fruitNames)");
+
+for name in fruitNames {
+    print("I like to eat \(name)");
+}
+
+print (fruitNames[1])
+
